8740ff14c1483d134eba99ec87d71857113bafc0
lictical <2683044588@qq.com>
2023-07-13 15:44:09 +0800

    zuoye

---
+++ Users/xiaoyi/cmake/foo.h
@@ -0,0 +1,6 @@
+#ifndef FOO_H
+#define FOO_H
+
+int some_function(int a, int b);
+
+#endif
---
+++ Users/xiaoyi/cmake/zuoye.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "foo.h"
+using namespace std;
+int main(){
+    cout<<"halloworld"<<endl;
+    cout << some_function(1,2);
+}
+
