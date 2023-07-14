f7ac4ca1bf1ca4efa68b02ac20fbaa77b7b44c33
lictical <2683044588@qq.com>
2023-07-14 13:28:29 +0800

    chehi

---
+++ Users/xiaoyi/cmake/build/CMakeFiles/demo.dir/foo.cpp.obj.d
@@ -0,0 +1,2 @@
+CMakeFiles/demo.dir/foo.cpp.obj: C:\Users\xiaoyi\cmake\foo.cpp \
+ C:\Users\xiaoyi\cmake\foo.h
--- Users/xiaoyi/cmake/zuoye.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "foo.h"
-using namespace std;
-int main(){
-    cout<<"halloworld"<<endl;
-    cout << some_function(1,2);
-}
-
